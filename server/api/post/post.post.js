4769e0b64255a7620d0b1688bb9e0fa8fcb64d29
zhukov <zhukov@etalon48.com>
2023-12-20 15:35:08 +0300

    yar77

--- server/api/post/post.post.js
+++ server/api/post/post.post.js
@@ -22,7 +22,7 @@ export default defineEventHandler(async(event) => {
           uploadDir: "./public/uploads",
           keepExtensions: true,
         });
-        console.log('n72')
+        console.log('n724')
       
         const body_files= files.image;
         let imagesArray = [];
